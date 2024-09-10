ea30692c7efa3fcfbb90d3d51735baf743c04cbf
Sachin Parihar <pariharsachin5002@gmail.com>
2024-09-10 21:51:41 +0530

    please chal ja

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,6 +1,6 @@
 const Header = () => {
     return(
-        <div className="h-[10vh] w-full bg-[#E4EFE7] flex items-center justify-between">
+        <div className="h-[10vh] w-full bg-white flex items-center justify-between">
             <p className="text-3xl text-[#064420] mx-6 font-semibold">Saya <span className="text-sm font-normal">- Saving memories</span></p>
         </div>
     )
